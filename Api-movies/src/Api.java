088cbbe82135951dbf60357759dfb95c41b25f37
Gustavo Jastrow <jaastrow@hotmail.com>
2023-03-31 22:53:43 -0300

    Imersão Aula 4 - Criando API de linguagens com Spring

--- Api-movies/src/Api.java
+++ Api-movies/src/Api.java
@@ -1,8 +1,11 @@
+import java.time.LocalDate;
+
 public enum Api {
     IMDB_TOP_MOVIES("https://raw.githubusercontent.com/alura-cursos/imersao-java-2-api/main/TopMovies.json", new ImDbContentExtractor()),
     IMDB_POPULARES_MOVIES("https://raw.githubusercontent.com/alura-cursos/imersao-java-2-api/main/MostPopularMovies.json", new ImDbContentExtractor()),
-    NASA("https://api.nasa.gov/planetary/apod?api_key=bx519SB8uKU36mYoAPSVeN6WU7gptf5neqY3PtdR&start_date=2023-01-10&end_date=2023-01-20", new NasaContentExtractor());
-    
+    NASA("https://api.nasa.gov/planetary/apod?api_key=bx519SB8uKU36mYoAPSVeN6WU7gptf5neqY3PtdR&start_date=2023-01-10&end_date=2023-01-20", new NasaContentExtractor()),
+    LOCAL("http://localhost:8080/linguagens", new LinguagensContentExtractor());
+
     private String url;
     private ContentExtractor contentExtractor;
 
@@ -10,7 +13,6 @@ public enum Api {
         this.url = url;
         this.contentExtractor = contentExtractor;
     }
-
 
     public ContentExtractor getContentExtractor() {
         return contentExtractor;
