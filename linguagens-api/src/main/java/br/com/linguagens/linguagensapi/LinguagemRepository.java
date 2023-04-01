8050cb5c1891515f3e5e18805e2e7cad3e7eea22
Gustavo Jastrow <jaastrow@hotmail.com>
2023-03-31 22:56:15 -0300

    Imersão Aula 4 - Criando API de linguagens com Spring

--- linguagens-api/src/main/java/br/com/linguagens/linguagensapi/LinguagemRepository.java
+++
@@ -1,9 +0,0 @@
-package br.com.linguagens.linguagensapi;
-
-import java.util.List;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface LinguagemRepository extends MongoRepository<Linguagem, String>{
-    List<Linguagem> findByOrderByRanking();
-}
