c5d9051125eb32fe79fce216d9501bb69560b7b0
Gustavo Jastrow <jaastrow@hotmail.com>
2023-03-31 22:49:31 -0300

    Imersão Aula 4 c:; cd 'c:\Users\Jaast\OneDrive\Área de Trabalho\ImersaoJava'; & 'C:\Users\Jaast\AppData\Local\Programs\Eclipse Adoptium\jdk-17.0.6.10-hotspot\bin\java.exe' '-XX:+ShowCodeDetailsInExceptionMessages' '-cp' 'C:\Users\Jaast\AppData\Roaming\Code\User\workspaceStorage\6e015c6c8e3a0912e317b28fb1caf9ca\redhat.java\jdt_ws\ImersaoJava_eac164a8\bin' 'App'  - Criando API com Spring Boot

---
+++ linguagens-api/src/main/java/br/com/linguagens/linguagensapi/LinguagemRepository.java
@@ -0,0 +1,9 @@
+package br.com.linguagens.linguagensapi;
+
+import java.util.List;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface LinguagemRepository extends MongoRepository<Linguagem, String>{
+    List<Linguagem> findByOrderByRanking();
+}
---
+++ linguagens-api/src/main/java/br/com/linguagens/linguagensapi/LinguagensApiApplication.java
@@ -0,0 +1,14 @@
+package br.com.linguagens.linguagensapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+
+public class LinguagensApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LinguagensApiApplication.class, args);
+	}
+
+}
